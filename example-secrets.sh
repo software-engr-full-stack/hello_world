0f951dbca7a3c567138f0da9c1800c878f73fa6e
software-engr-full-stack <software.engr.full.stack@gmail.com>
2023-03-13 18:22:25 -0700

    Add config and secrets example

---
+++ example-secrets.sh
@@ -0,0 +1,10 @@
+export RAILS_ENV=production
+export HELLO_WORLD_DATABASE_HOST=server.rds.amazonaws.com
+export HELLO_WORLD_DATABASE_NAME=db_name
+export HELLO_WORLD_DATABASE_USER=username
+export HELLO_WORLD_DATABASE_PASSWORD=password
+export SECRET_KEY_BASE=skb
+
+# https://stackoverflow.com/a/63408595
+# rm -rf config/credentials.yml.enc
+# EDITOR="vi --wait" bin/rails credentials:edit
